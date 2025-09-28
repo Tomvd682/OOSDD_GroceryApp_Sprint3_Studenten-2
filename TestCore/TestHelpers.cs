2c67862aff329eaee77ff1bff8349ea44fe044ea
Tomvd682 <146827201+Tomvd682@users.noreply.github.com>
2025-09-28 02:38:30 +0200

    Unhappy path uitgewerkt voor w8woordhHelper
    Assert.IsFalse toegevoegd voor verkeerde wachtwoorden en corrupte hashes; vervangt placeholders Assert.Pass/Fail.

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -1,16 +1,14 @@
 using Grocery.Core.Helpers;
+using NUnit.Framework; // zorg dat deze erbij staat
 
 namespace TestCore
 {
     public class TestHelpers
     {
         [SetUp]
-        public void Setup()
-        {
-        }
+        public void Setup() { }
 
-
-        //Happy flow
+        // Happy flow
         [Test]
         public void TestPasswordHelperReturnsTrue()
         {
@@ -26,19 +24,22 @@ namespace TestCore
             Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
-
-        //Unhappy flow
+        // Unhappy flow Aangevuld met ecjte test
         [Test]
         public void TestPasswordHelperReturnsFalse()
         {
-            Assert.Pass(); //Zelf uitwerken
+            // Verkeerd wachtwoord bij een geldige hash => moet false zijn
+            string wrongPassword = "user1_WRONG";
+            string validHashForUser1 = "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08=";
+            Assert.IsFalse(PasswordHelper.VerifyPassword(wrongPassword, validHashForUser1));
         }
 
+        // Hashes hieronder missen de trailing '=' (bewust corrupt) -> moet false zijn
         [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
         public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
         {
-            Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
         }
     }
 }
